8ec395eccadfca47ab1788dffeab31ef7aef0182
mukang <mukang.max@foxmail.com>
2018-07-16 22:27:33 +0800

    new

--- Homepwner/Homepwner/ImageStore.swift
+++ Homepwner/Homepwner/ImageStore.swift
@@ -12,13 +12,47 @@ import UIKit
 class ImageStore {
     let cache = NSCache<AnyObject, AnyObject>()
     
-    func setImage(image: UIImage, forKey key: NSString) {
-        cache.setObject(image, forKey: key)
+    func setImage(image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: NSString(string: key))
+        
+        let imageURL = imageURLForKey(key: key)
+        if let data = UIImageJPEGRepresentation(image, 0.5) {
+            do {
+                try data.write(to: imageURL)
+            } catch {
+                print(error)
+            }
+        }
     }
-    func imageForKey(key: NSString) -> UIImage? {
-        return cache.object(forKey: key) as? UIImage
+    
+    func imageForKey(key: String) -> UIImage? {
+        if let existingImage = cache.object(forKey: NSString(string: key)) as? UIImage {
+            return existingImage
+        }
+        
+        let imageURL = imageURLForKey(key: key)
+        guard let imageFromDisk = UIImage(contentsOfFile: imageURL.path) else {
+            return nil
+        }
+        
+        cache.setObject(imageFromDisk, forKey: NSString(string: key))
+        return imageFromDisk
     }
-    func deleteImageForKey(key: NSString) {
-        cache.removeObject(forKey: key)
+    
+    func deleteImageForKey(key: String) {
+        cache.removeObject(forKey: NSString(string: key))
+        
+        let imageURL = imageURLForKey(key: key)
+        do {
+            try FileManager.default.removeItem(at: imageURL)
+        } catch let deleteError {
+            print("Error removing the image from disk: \(deleteError)")
+        }
+    }
+    
+    func imageURLForKey(key: String) -> URL {
+        let documentsDirctories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirctories.first!
+        return documentDirectory.appendingPathComponent(key)
     }
 }
--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ItemStore {
     var allItems = [Item]()
+    let itemArchiveURL: URL = {
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("items.archive")
+    }()
+    
+    init() {
+        if let archivedItems = NSKeyedUnarchiver.unarchiveObject(withFile: itemArchiveURL.path) as? [Item] {
+            allItems += archivedItems
+        }
+    }
     
     func createItem() -> Item {
         let newItem = Item(random: true)
@@ -31,5 +42,10 @@ class ItemStore {
         let movedItem = allItems[fromIndex]
         allItems.remove(at: fromIndex)
         allItems.insert(movedItem, at: toIndex)
+    }
+    
+    func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL.path)")
+        return NSKeyedArchiver.archiveRootObject(allItems, toFile: itemArchiveURL.path)
     }
 }
